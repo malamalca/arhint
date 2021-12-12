442476665e8b7a6d024c6ffa657361a96f2673b6
Miha Nahtigal <miha.nahtigal@gmail.com>
2021-12-12 18:12:14 +0100

    Dev

---
+++ plugins/LilInvoices/config/Migrations/20211212082524_RemoveLayoutAndLayoutTitleFromInvoicesCounters.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class RemoveLayoutAndLayoutTitleFromInvoicesCounters extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('invoices_counters');
+        $table->removeColumn('layout_title');
+        $table->removeColumn('layout');
+        $table->update();
+    }
+}
