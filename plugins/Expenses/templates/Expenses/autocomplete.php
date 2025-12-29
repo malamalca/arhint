6a7bdedb62f9018af4e26238b799a12c72382e29
Malamalca <miha@malamalca.com>
2025-12-29 11:03:24 +0100

    Expenses autocomplete fix

--- plugins/Expenses/templates/Expenses/autocomplete.php
+++ plugins/Expenses/templates/Expenses/autocomplete.php
@@ -2,15 +2,17 @@
 $this->loadHelper('Expenses.LilExpense');
 
 $ret = [];
-foreach ($expenses as $exp) {
-    $ret[] = [
-        'id' => $exp->id,
-        'text' => $this->LilExpense->label($exp),
-        'value' => $this->LilExpense->title($exp),
-        'icon' => $this->LilExpense->icon($exp),
-        'title' => $exp->title,
-        'total' => $exp->total,
-    ];
+if ($expenses) {
+    foreach ($expenses as $exp) {
+        $ret[] = [
+            'id' => $exp->id,
+            'text' => $this->LilExpense->label($exp),
+            'value' => $this->LilExpense->title($exp),
+            'icon' => $this->LilExpense->icon($exp),
+            'title' => $exp->title,
+            'total' => $exp->total,
+        ];
+    }
 }
 
 echo json_encode($ret);
